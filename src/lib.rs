b77cdfd224f6f51ba439803135cb5d093715e872
happy <golinuxlove@gmail.com>
2022-09-12 18:00:50 +0800

    去掉不需要的文件

--- src/lib.rs
+++ src/lib.rs
@@ -8,39 +8,37 @@
 #![feature(generic_const_exprs)]
 #![feature(default_free_fn)]
 #![feature(exclusive_range_pattern)]
+#[macro_use]
+extern crate approx;
 #[allow(dead_code, unused_imports, unused_variables, unused_imports, unused, missing_docs, unused_results, unused_must_use)]
 #[allow(unused_mut)]
 #[macro_use]
 extern crate bitflags;
+extern crate core;
 #[macro_use]
 extern crate derivative;
-#[macro_use]
-extern crate serde;
-#[macro_use]
-extern crate approx;
+extern crate hash32;
 #[macro_use]
 extern crate hash32_derive;
-extern crate hash32;
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate serde;
 
 use std::collections::HashSet;
 use std::error::Error;
-use crate::parsed_data::GeomsInfo;
+use std::time::Instant;
+use aios_core::tool::db_tool::db1_hash;
 use futures::stream::TryStreamExt;
+
+pub use parse::{parse_db, parse_file};
 pub use parse::parse_pdms_dir;
 
 pub mod error_types;
 pub mod test_cases;
 pub mod parse_explict_tools;
-pub mod parsed_data;
 pub mod parse;
 pub mod consts;
-pub mod shape;
-pub mod grpc;
-
-pub use parse::{parse_db, parse_file};
-use std::time::Instant;
-use aios_core::tool::db_tool::db1_hash;
-
 pub mod options;
 
 pub type BHashMap<K, V> = bevy::utils::HashMap<K, V>;
@@ -82,9 +80,9 @@ const ATT_PXTS: i32 = 0xFFF1F3AAu32 as i32;
 const ATT_PYTS: i32 = 0xFFF1F38Fu32 as i32;
 const ATT_PXBS: i32 = 0xFFF226ECu32 as i32;
 const ATT_PYBS: i32 = 0xFFF226D1u32 as i32;
-const ATT_PRAD : i32 = db1_hash("PRAD") as i32;
-const ATT_DRAD : i32 = db1_hash("DRAD") as i32;
-const ATT_PWID : i32 = db1_hash("PWID") as i32;
+const ATT_PRAD: i32 = db1_hash("PRAD") as i32;
+const ATT_DRAD: i32 = db1_hash("DRAD") as i32;
+const ATT_PWID: i32 = db1_hash("PWID") as i32;
 
 const ATT_PANG: i32 = 0xA5E2F;
 const IMP_PAXI: i32 = 0xB146F;
@@ -120,10 +118,6 @@ const IMP_PXTS: i32 = 0xE0C56;
 const IMP_PYTS: i32 = 0xE0C71;
 const IMP_PXBS: i32 = 0xDD914;
 const IMP_PYBS: i32 = 0xDD92F;
-
-#[macro_use]
-extern crate lazy_static;
-extern crate core;
 
 lazy_static! {
     pub static ref EXPR_ATT_SET: HashSet<i32> = {
--- src/parse_increment_data/mod.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(not(target_arch = "wasm32"))]
-pub mod increment_modify;
-
-pub enum NewDataState {
-    Modify,
-    Increase,
-    Delete
-}
