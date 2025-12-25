4ada32160e07225dabe41d926faf4935a901f3a3
dongpengcheng <happydpc@sina.com>
2025-12-25 21:47:32 +0800

    refactor: 清理 lib.rs 中未使用的 macro_use 和稳定 feature
    - 移除稳定 feature: array_methods, associated_type_bounds
    - 移除未使用 macro_use: approx, bitflags, derivative, hash32, hash32_derive
    - 警告数 91 -> 86

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,14 @@
 #![feature(type_ascription)]
-#![feature(array_methods)]
 #![feature(slice_pattern)]
-#![feature(associated_type_bounds)]
 #![feature(generic_const_exprs)]
 #![allow(incomplete_features)]
-#[macro_use]
-extern crate approx;
-#[allow(
-    dead_code,
-    unused_imports,
-    unused_variables,
-    unused,
-    missing_docs,
-    unused_results,
-    unused_must_use
-)]
-#[allow(unused_mut)]
-#[macro_use]
-extern crate bitflags;
-extern crate core;
-#[macro_use]
-extern crate derivative;
-extern crate hash32;
-#[macro_use]
-extern crate hash32_derive;
+
 #[macro_use]
 extern crate lazy_static;
 #[macro_use]
 extern crate serde;
+
+extern crate core;
 
 
 pub use parse::parse_pdms_dir;
